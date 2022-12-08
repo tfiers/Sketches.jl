78baf6bed8568303f5788a096ae0b0f8f467677c
Documenter.jl <documenter@juliadocs.github.io>
2022-12-08 23:27:16 +0000

    build based on bf0640b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Sketches","category":"page"},{"location":"#Sketches","page":"Home","title":"Sketches","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Sketches.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Sketches]","category":"page"}]
+}
